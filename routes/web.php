b9b263bc56c7a45047a6c6fccd25e6f574394665
Reymark Torres <rtorres@i4asiacorp.com>
2017-11-18 21:43:09 +0800

    Alter Laravel exception handling and create the threads creation page for authenticated users

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/threads', 'ThreadsController@index');
-Route::post('/threads', 'ThreadsController@store');
-Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::resource('threads', 'ThreadsController');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
 
 Auth::routes();
