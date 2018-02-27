764d3b04067d1218fe6109d0ed6fd7089818f166
reymark <reymark@kickstart.intranet.i4asiacorp.com>
2018-02-27 15:35:24 +0800

    Create notification fakes in ThreadTest class

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
+use App\Notifications\ThreadWasUpdated;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
 
 class ThreadTest extends TestCase
 {
@@ -50,6 +52,22 @@ class ThreadTest extends TestCase
         ]);
 
         $this->assertCount(1, $this->thread->replies);
+    }
+
+    /** @test */
+    function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+
+        $this->signIn()
+             ->thread
+             ->subscribe()
+             ->addReply([
+                 'body' => 'Foobar',
+                 'user_id' => 999
+             ]);
+
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated::class);
     }
 
     /** @test */
